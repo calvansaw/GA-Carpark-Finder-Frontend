423ef2edc3b5355921206c6fd3793cd8d3e5eb92
calvansaw <calvansaw@hotmail.com>
2021-04-01 19:44:22 +0800

    frontend debug

--- carpark_app_frontend/src/App.js
+++ carpark_app_frontend/src/App.js
@@ -1,73 +1,160 @@
-import React from 'react';
-import '../src/App.css';
-import { Login, Register } from './components/index';
+import React from 'react'
+import Display from './Display'
+
+import './App.css';
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeLogin: true,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+            area: 'redhill',
+            fetchedArea: false,
+            fetchedAvailability: '',
+            userID:''
+        };
+	}
 
-  componentDidMount() {
-    this.rightSide.classList.add('right');
-  }
-
-  changeState() {
-    const { activeLogin } = this.state;
-
-    if (activeLogin) {
-      this.rightSide.classList.remove('right');
-      this.rightSide.classList.add('left');
-    } else {
-      this.rightSide.classList.remove('left');
-      this.rightSide.classList.add('right');
+    fetchData = () => {
+        fetch('https://parkit-sg.herokuapp.com/carparkavailability')
+        .then((response) => {
+            console.log(response);
+            return response.json()
+        })
+        .then((fetchedData) => {
+            this.setState({fetchedAvailability: fetchedData.items[0].carpark_data})
+            console.log(this.state.fetchedAvailability);
+            
+        }).catch(err => console.log(err));
     }
-    this.setState((prevState) => ({
-      activeLogin: !prevState.activeLogin,
-    }));
-  }
 
-  render() {
-    const { activeLogin } = this.state;
-    const current = activeLogin ? 'Register' : 'Login';
-    const currentActive = activeLogin ? 'login' : 'register';
-    return (
-      <div className='App'>
-        <div className='login'>
-          <div className='container' ref={(ref) => (this.container = ref)}>
-            {activeLogin && (
-              <Login containerRef={(ref) => (this.current = ref)} />
-            )}
-            {!activeLogin && (
-              <Register containerRef={(ref) => (this.current = ref)} />
-            )}
-          </div>
-          <RightSide
-            current={current}
-            currentActive={currentActive}
-            containerRef={(ref) => (this.rightSide = ref)}
-            onClick={this.changeState.bind(this)}
-          />
-        </div>
-      </div>
-    );
-  }
+    fetchOtherData = () => {
+        fetch('https://parkit-sg.herokuapp.com/carparkdetails?area=' + this.state.area)
+        .then((response) => {
+            console.log(response);
+            return response.json()
+        })
+        .then((fetchedDetails) => {
+            this.setState({fetchedArea: fetchedDetails})
+            console.log(this.state.fetchedArea);
+            
+        }).catch(err => console.log(err));
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.id]: event.target.value})
+    }
+
+    handleLogin = (event) => {
+        event.preventDefault();
+        const loginUserData = new FormData(event.target);
+        console.log(event.target)
+        console.log(loginUserData.get('username'))
+        fetch('https://parkit-sg.herokuapp.com/sessions', {
+            body: JSON.stringify({
+                username: loginUserData.get('username'),
+                password: loginUserData.get('password')
+            }),
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+				'Content-Type': 'application/json',
+            },
+        })
+            .then((loginUser) => {
+              console.log(loginUser);
+              return loginUser.json()
+            })
+            .then((jsonedUser) => {
+                console.log(jsonedUser);
+                this.setState({userID: jsonedUser._id})
+                this.fetchOtherData()
+            })
+            .catch(error => console.log(error));
+        event.target.reset();
+
+
+    }
+
+    handleCreateUser = (event) => {
+        event.preventDefault();
+        const createUserData = new FormData(event.target);
+        console.log(event.target)
+        console.log(createUserData.get('username'))
+        fetch('https://parkit-sg.herokuapp.com/users', {
+            body: JSON.stringify({
+                username: createUserData.get('username'),
+                password: createUserData.get('password')
+            }),
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+				        'Content-Type': 'application/json',
+            },
+        })
+            .then(createdUser => createdUser.json())
+            .then((jsonedUser) => {
+                console.log(jsonedUser);
+            })
+            .catch(error => console.log(error));
+        event.target.reset();
+
+    }
+
+    handleLogout = () => {
+        fetch('/sessions', {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+            .then((jsonedResponse) => {
+                this.setState({fetchedArea: ''})
+                console.log(jsonedResponse);
+            })
+            .catch(error => console.log(error));
+    }
+
+    componentDidMount() {
+        this.fetchData()
+        this.fetchOtherData()
+    }
+
+
+	render() {
+		return (
+            <React.Fragment>
+                <form onSubmit={this.handleCreateUser}>
+                        <label htmlFor="create-username"></label>
+                        <input type="text" id="create-username" name="username" placeholder="Input Username here" onChange={this.handleChange}></input>
+                        <label htmlFor="create-password"></label>
+                        <input type="password" id="create-password" name="password" placeholder="Enter Password here" onChange={this.handleChange}></input>
+                        <input type="submit" value="Create User"></input>
+                </form>    
+                    
+                <form onSubmit={this.handleLogin}>
+                        <label htmlFor="login-username"></label>
+                        <input type="text" id="login-username" name="username" placeholder="Input Username here" onChange={this.handleChange}></input>
+                        <label htmlFor="login-password"></label>
+                        <input type="password" id="login-password" name="password" placeholder="Enter Password here" onChange={this.handleChange}></input>
+                        <input type="submit" value="Login"></input>
+                </form>
+
+                <button onClick={this.handleLogout}>Logout</button>
+
+                {this.state.fetchedArea ?
+                <div>
+                    Test
+                    <button onClick={this.fetchData}>DataFromGovAPI</button>
+                    <button onClick={this.fetchOtherData}>OtherDataFromMongoDB</button>
+                    <br></br>
+                    
+                    'Appear if state has value...'
+                    <Display area={this.state.fetchedArea} detail={this.state.fetchedAvailability} />
+                </div>
+                : 'State has no value'
+                }
+            </React.Fragment>
+		);
+	}
 }
-
-const RightSide = (props) => {
-  return (
-    <div
-      className='right-side'
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className='inner-container'>
-        <div className='text'>{props.current}</div>
-      </div>
-    </div>
-  );
-};
 
 export default App;
